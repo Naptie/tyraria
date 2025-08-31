385e1867763a35e72bec7a6b43d58015a250bd8b
Naptie <naptie@qq.com>
2025-08-31 16:59:06 +0800

    chore: rename main.css to app.css

--- src/routes/+layout.js
+++
@@ -1,3 +0,0 @@
-import '../main.css';
-import '@vscode-elements/elements-lite/components/action-button/action-button.css';
-import '@vscode-elements/elements-lite/components/button/button.css';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
+<script>
+  import "../app.css";
+  import "@vscode-elements/elements-lite/components/action-button/action-button.css";
+  import "@vscode-elements/elements-lite/components/button/button.css";
+</script>
+
 <slot />
 
 <style>
